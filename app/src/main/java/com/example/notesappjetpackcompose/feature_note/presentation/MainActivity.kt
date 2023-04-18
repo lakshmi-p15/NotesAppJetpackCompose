d5e29bb48ea5b7b8c8652c0d5c1a2b3857b7ca29
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2023-04-18 09:03:43 -0700

    Database and Repository setup

--- app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.notesappjetpackcompose
+package com.example.notesappjetpackcompose.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
