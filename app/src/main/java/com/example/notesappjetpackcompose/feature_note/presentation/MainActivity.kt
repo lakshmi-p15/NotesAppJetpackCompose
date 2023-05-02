3a537a3dfb95422a561f0fb883ead3856cfb7107
lakshmi-p15 <lakshmi.pavuluri15@gmail.com>
2023-05-02 10:18:22 -0700

    Complete UI and Implementation

--- app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/MainActivity.kt
@@ -10,14 +10,61 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.notesappjetpackcompose.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.example.notesappjetpackcompose.feature_note.presentation.notes.NotesScreen
+import com.example.notesappjetpackcompose.feature_note.presentation.util.Screen
 import com.example.notesappjetpackcompose.ui.theme.NotesAppJetpackComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppJetpackComposeTheme {
+                Surface(
+                    color = MaterialTheme.colors.background
+                ){
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route,
+                        ){
+                        composable(route = Screen.NotesScreen.route){
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                    "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ){
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ){
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ){
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
 
+                }
             }
         }
     }
--- app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/example/notesappjetpackcompose/feature_note/presentation/notes/NotesScreen.kt
@@ -18,6 +18,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.notesappjetpackcompose.feature_note.presentation.notes.components.NoteItem
 import com.example.notesappjetpackcompose.feature_note.presentation.notes.components.OrderSection
+import com.example.notesappjetpackcompose.feature_note.presentation.util.Screen
 import kotlinx.coroutines.launch
 
 @Composable
@@ -32,7 +33,7 @@ fun NotesScreen(
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = {
-
+                navController.navigate(Screen.AddEditNoteScreen.route)
             },
             backgroundColor = MaterialTheme.colors.primary
             ) {
@@ -88,7 +89,10 @@ fun NotesScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .clickable {
-
+                                navController.navigate(
+                                    Screen.AddEditNoteScreen.route +
+                                            "?noteId=${note.id}&noteColor=${note.color}"
+                                )
                             },
                         onDeleteClick = {
                             viewModel.onEvent(NotesEvent.DeleteNote(note))
